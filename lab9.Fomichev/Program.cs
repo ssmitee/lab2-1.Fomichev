3c81fa293e3742e49fe3050ab94f3ca2f247fb9b
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-23 20:44:45 +0200

    Добавьте файлы проекта.

---
+++ lab9.Fomichev/Program.cs
@@ -0,0 +1,13 @@
+﻿decimal cost = 1500;
+decimal discount = 0;
+if (cost > 1000 && cost < 2000)
+{
+    discount = cost * 0.05m;
+}
+else if (cost >= 2000 && cost < 5000)
+{
+    discount = cost * 0.1m;
+}
+decimal finalCost = cost - discount;
+
+Console.WriteLine($"Итоговая стоимость покупки: {finalCost} грн");
